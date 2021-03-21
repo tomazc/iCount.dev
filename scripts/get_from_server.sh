9f94a7a57c93b59c36b5962a88cc121a8c84ad04
Tomaž Curk <tomazc@gmail.com>
2021-03-21 19:28:01 +0100

    Add analysis info

---
+++ scripts/get_from_server.sh
@@ -0,0 +1,2 @@
+scp icuser@icount.biolab.si:export/exp2json.py .
+scp icuser@icount.biolab.si:"export/*.json" ../data
--- scripts/process_json.py
+++ scripts/process_json.py
@@ -15,3 +15,20 @@ type: "experiment"
     f = open("../content/experiments/%s.html" % exp_id, "wt")
     f.write(code)
     f.close()
+
+
+data = json.load(open("../data/analysis.json"))
+for ana_id, ana_data in data.items():
+    # write experiment html file
+    code = """
+---
+title: {ana_id}
+linkTitle: {ana_id}
+type: "page"
+analysis: "{ana_id}"
+type: "analysis"
+---
+""".format(ana_id=ana_id)
+    f = open("../content/analysis/%s.html" % ana_id, "wt")
+    f.write(code)
+    f.close()
